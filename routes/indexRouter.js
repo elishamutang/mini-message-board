e5d8a1fffc3b96a098fe3f790beba7c46ca21ee2
elisha <elishamutang@outlook.com>
2025-03-22 17:46:57 +0800

    Added form to submit new message.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -20,4 +20,16 @@ indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
+indexRouter.get("/new", (req, res) => {
+  res.render("form");
+});
+
+indexRouter.post("/new", (req, res) => {
+  messages.push({ text: req.body.message, user: req.body.name, added: format(new Date(), "PPPPpppp") });
+
+  setTimeout(() => {
+    res.redirect("/");
+  }, 1000);
+});
+
 export default indexRouter;
