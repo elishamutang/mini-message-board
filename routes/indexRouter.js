f6c50e2325f6f897994e0a8d76c53881dedcf0d8
elisha <elishamutang@outlook.com>
2025-03-24 22:53:05 +0800

    Added ID property for each message. Added new request handlers for opening each message and error handling.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { format } from "date-fns";
 
+const title = "Mini Message Board";
+
 const messages = [
   {
+    id: 1,
     text: "Hi there!",
     user: "Amando",
     added: format(new Date(), "PPPPpppp"),
   },
   {
+    id: 2,
     text: "Hello World!",
     user: "Charles",
     added: format(new Date(), "PPPPpppp"),
@@ -17,7 +21,14 @@ const messages = [
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
-  res.render("index", { title: "Mini Message Board", messages: messages });
+  res.render("index", { title: title, messages: messages });
+});
+
+indexRouter.get("/messages/:messageId", (req, res) => {
+  const { messageId } = req.params;
+  const requestedMsg = messages.find((message) => message.id === Number(messageId));
+
+  res.render("message", { title: title, message: requestedMsg });
 });
 
 indexRouter.get("/new", (req, res) => {
@@ -25,11 +36,20 @@ indexRouter.get("/new", (req, res) => {
 });
 
 indexRouter.post("/new", (req, res) => {
-  messages.push({ text: req.body.message, user: req.body.name, added: format(new Date(), "PPPPpppp") });
+  messages.push({
+    id: messages.length + 1,
+    text: req.body.message,
+    user: req.body.name,
+    added: format(new Date(), "PPPPpppp"),
+  });
 
   setTimeout(() => {
     res.redirect("/");
   }, 1000);
+});
+
+indexRouter.all("*", (req, res) => {
+  res.render("error", { errorMsg: "ERROR, INVALID URL :/" });
 });
 
 export default indexRouter;
